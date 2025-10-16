664ae5909584a861a038f30069974ef7ab738a4b
Suraj Dombale <148750986+surajdombale@users.noreply.github.com>
2025-10-16 15:13:29 +0530

    Delete springboot-jpa directory

--- springboot-jpa/src/main/java/com/example/springbootjpa/repository/StudentRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.springbootjpa.repository;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface StudentRepo extends JpaRepository<com.example.springbootjpa.entity.Student,Integer>{
-	
-}
